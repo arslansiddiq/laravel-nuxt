1d23f621a07146402edf16fa3fb88321fbe18357
arslan Siddique <arsi.555@gmail.com>
2018-06-02 01:32:30 +0500

    Inital Setup Errors Resolved

--- client/components/global/index.js
+++ client/components/global/index.js
@@ -3,7 +3,7 @@ import Vue from 'vue'
 const requireContext = require.context('./', false, /.*\.(vue)$/)
 
 requireContext.keys().forEach(file => {
-  const Component = requireContext(file).default
+  const Component = requireContext(file)
 
   if (Component.name) {
     Vue.component(Component.name, Component)
